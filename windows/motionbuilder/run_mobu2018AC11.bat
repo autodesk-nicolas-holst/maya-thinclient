6b123a3fd87844dbf945ce5b665c776ea9157344
autodesk-nicolas-holst <41784931+autodesk-nicolas-holst@users.noreply.github.com>
2019-06-03 14:42:49 +0100

    Add files via upload

---
+++ windows/motionbuilder/run_mobu2018AC11.bat
@@ -0,0 +1,4 @@
+set BASEFOLDER=D:\d\thin_client\c_programfiles_autodesk\MotionBuilder2018AC11
+set AUTODESK_ADLM_THINCLIENT_ENV=D:\d\thin_client\run_mobu2018AC11.xml
+set ADSKFLEX_LICENSE_SERVER=2080@10.146.127.212
+%BASEFOLDER%\bin\x64\motionbuilder.exe
